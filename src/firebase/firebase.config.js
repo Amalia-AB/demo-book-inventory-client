582361e2869f923e8d4975b51c33ad70097073f0
Amalia-AB~ <amalia.ab03@gmail.com>
2024-06-11 12:43:48 +0300

    firebase.config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCIQl0weHWkyX7FNmGNCTElUYL9aL0HPMA",
-    authDomain: "online-book-store-78516.firebaseapp.com",
-    projectId: "online-book-store-78516",
-    storageBucket: "online-book-store-78516.appspot.com",
-    messagingSenderId: "282181520275",
-    appId: "1:282181520275:web:0ce28538cea0433ea83666"
-  };
+  apiKey: "AIzaSyDwCXbrqZIsOfasg_NcapIWl6wMuv670RM",
+  authDomain: "fir-book-inventory-eee14.firebaseapp.com",
+  projectId: "fir-book-inventory-eee14",
+  storageBucket: "fir-book-inventory-eee14.appspot.com",
+  messagingSenderId: "819751430717",
+  appId: "1:819751430717:web:0f1f2edd5b1b5cf36b9854"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
